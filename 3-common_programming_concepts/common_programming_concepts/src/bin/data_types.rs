5e861ad17fb1337fa8305fb5a9f5ce38403918e1
HamidrezaSK <shrsanaee@gmail.com>
2024-01-11 14:54:07 +0000

    Initializing part 3 of the rust book and having all of the sub sections in one projcet

---
+++ 3-common_programming_concepts/common_programming_concepts/src/bin/data_types.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let tup = (500, 6.4, 1);
+
+    let (_, y, _) = tup;
+
+    println!("The value of y is: {y}");
+    println!("The value of tup.0 is: {}",tup.0);
+    let a = [3; 5];
+    let b: [i32; 5] = [1, 2, 3, 4, 5];
+
+}
