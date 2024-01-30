9c3eb996a385993ae36a9b023f07e5fe08da4e13
HamidrezaSK <shrsanaee@gmail.com>
2024-01-30 16:01:03 +0000

    Update reference

--- 4-ownership/ownership/src/bin/reference.rs
+++ 4-ownership/ownership/src/bin/reference.rs
@@ -1,13 +1,15 @@
 fn main() {
     let mut s = String::from("hello");
 
-    let r1 = &mut s;
-    let r2 = &mut s;
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+    println!("{} and {}", r1, r2);
+    // variables r1 and r2 will not be used after this point
 
-    println!("{},", r1);
-    println!("{},", r2);
+    let r3 = &mut s; // no problem
+    println!("{}", r3);
 }
 
-fn change(some_string: &mut String) {
-    some_string.push_str(", world");
-}
+// fn change(some_string: &mut String) {
+//     some_string.push_str(", world");
+// }
