51991e2f45d6da63ab3741bdef145b3f02ad92f3
HamidrezaSK <shrsanaee@gmail.com>
2024-02-13 15:21:00 +0000

    Completed the first linkedlist with tests

--- datastructures/lists/src/first.rs
+++ datastructures/lists/src/first.rs
@@ -1,5 +1,38 @@
 use std::mem;
 
+#[cfg(test)]
+mod test{
+    use super::List;
+    #[test]
+    fn basics() {
+        let mut list = List::new();
+
+        // Check empty list behaves right
+        assert_eq!(list.pop(), None);
+
+        // Populate list
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+
+        // Push some more just to make sure nothing's corrupted
+        list.push(4);
+        list.push(5);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), Some(4));
+
+        // Check exhaustion
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
+    }
+}
+
 pub struct List {
     head: Link,
 }
@@ -34,6 +67,17 @@ impl List{
                 self.head = node.next;
                 Some(node.elem)
             }
+        }
+
+    }
+}
+
+impl Drop for List{
+    fn drop(&mut self){
+        let mut cur_link = mem::replace(&mut self.head, Link::Empty);
+        
+        while let Link::More(mut boxed_node) = cur_link {
+            cur_link = mem::replace(&mut boxed_node.next, Link::Empty);
         }
 
     }
